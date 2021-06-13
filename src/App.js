b80c64768a917138742cbc2d15f754821839bcf4
Phongtlam <Phongtlam@gmail.com>
2021-06-13 12:09:28 -0700

    change table

--- src/App.js
+++ src/App.js
@@ -5,26 +5,17 @@ import Topics from "./components/Topics";
 import React from "react";
 
 const App = () => {
-  console.log('public url', process.env.PUBLIC_URL)
-
-  // const history = createHistory({
-  //   basename: process.env.PUBLIC_URL,
-  // });
-
   return (
     <div className="App">
       <Router basename={process.env.PUBLIC_URL}>
         <Topics />
         <div>
           <Switch>
-            <Route path="/topic/:name">
-              <ProblemsDisplay />
-            </Route>
             <Route path="/topics/:names">
               <ProblemsDisplay />
             </Route>
             <Route path="*">
-              <Home />
+              <div>Welcome</div>
             </Route>
           </Switch>
         </div>
